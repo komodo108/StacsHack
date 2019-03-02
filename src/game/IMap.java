97c590b0cd45446616e595ec5920737bf8cf4c88
pag5 <pag5@st-andrews.ac.uk>
2019-03-02 14:42:14 +0000

    Updated interfaces

--- src/game/IMap.java
+++ src/game/IMap.java
@@ -1,6 +1,15 @@
 package game;
 
+import game.helper.Position;
+
 public interface IMap {
     ITile[][] getMap();
 
+    void updateTileAt(Position pos);
+
+    ITile getTileAt(Position pos);
+
+    void generateMap();
+
+    Position getSingleTile(ITile tile);
 }
--- src/game/ITile.java
+++ src/game/ITile.java
@@ -1,5 +1,7 @@
 package game;
 
 public interface ITile {
-
+    // TODO: Not needed!
+    // There is only 1 type of tile
+    // Replace with Tile class
 }
