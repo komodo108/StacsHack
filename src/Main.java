b3ad7c7ca078f898bfc8d49deac45a6398a600af
pag5 <pag5@st-andrews.ac.uk>
2019-03-02 14:34:29 +0000

    More things working

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import views.ConnectionView;
 public class Main {
 
     public static void main(String[] args) {
-        new ConnectionView();
+        new ConnectionView(true);
     }
 
 }
---
+++ src/game/render/Render.java
@@ -0,0 +1,15 @@
+package game.render;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Render extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Paint
+    }
+
+    // TODO: Other methods
+}
